cc579379a813b4734c554da735a6b17938f02130
Paweł Feruś <null@mindc.net>
2017-04-08 13:27:13 +0200

    gofmt

--- api/get_ip.go
+++ api/get_ip.go
@@ -4,11 +4,11 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-// GetIP return remote ip address as string 
+// GetIP return remote ip address as string
 func GetIP(ctx *fasthttp.RequestCtx) string {
 	i := string(ctx.Request.Header.Peek("X-Forwarded-For"))
 	if len(i) > 0 {
-	    return i
+		return i
 	}
 	return ctx.RemoteIP().String()
 }
